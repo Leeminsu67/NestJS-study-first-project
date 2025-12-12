4778390c5950a78ca1a721b876af19eb854b0b9b
mingdev <alstn67@gmail.com>
2025-12-12 22:08:20 +0900

    NestJS postgresql 연동(Joi, env 사용)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,39 @@
 import { Module } from '@nestjs/common';
 import { MovieModule } from './movie/movie.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [MovieModule],
+  imports: [
+    // 환경변수 설정 및 확인
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_TYPE: Joi.string().valid('postgres').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: configService.get<string>('DB_TYPE') as 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [],
+        // 개발에서만 틀어놓고 실제 운영 환경에서는 끈다
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    MovieModule,
+  ],
 })
 export class AppModule {}
