e82849e601c9661efc7b30f31fb3f1d51dcf5dd1
mingdev <alstn67@gmail.com>
2025-12-15 17:20:02 +0900

    Genre CRUD 구현

--- src/genre/dto/update-genre.dto.ts
+++ src/genre/dto/update-genre.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateGenreDto } from './create-genre.dto';
+import { IsNotEmpty, IsOptional } from 'class-validator';
 
-export class UpdateGenreDto extends PartialType(CreateGenreDto) {}
+export class UpdateGenreDto extends PartialType(CreateGenreDto) {
+  @IsNotEmpty()
+  @IsOptional()
+  name?: string;
+}
