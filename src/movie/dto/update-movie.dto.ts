01ee99376d7116032963a52957b50fc662cc8430
mingdev <alstn67@gmail.com>
2025-12-18 00:16:42 +0900

    Mapped Types 적용

--- src/movie/dto/update-movie.dto.ts
+++ src/movie/dto/update-movie.dto.ts
@@ -6,35 +6,31 @@ import {
   IsOptional,
   IsString,
 } from 'class-validator';
+import { CreateMovieDto } from './create-movie.dto';
+import { PartialType } from '@nestjs/mapped-types';
 
-enum MovieGenre {
-  ACTION = 'action',
-  FANTASY = 'fantasy',
-}
-export class UpdateMovieDto {
-  @IsNotEmpty()
-  @IsString()
-  @IsOptional()
-  title?: string;
-
-  @IsNotEmpty()
-  @IsString()
-  @IsOptional()
-  detail?: string;
-
-  @IsNotEmpty()
-  @IsNumber()
-  @IsOptional()
-  directorId?: number;
-
-  @IsArray()
-  @ArrayNotEmpty()
-  @IsNumber(
-    {},
-    {
-      each: true,
-    },
-  )
-  @IsOptional()
-  genreIds?: number[];
+// PartialType이 CreateMovieDto에 있는 것을 다 옵셔널로 바꿔준다
+export class UpdateMovieDto extends PartialType(CreateMovieDto) {
+  // @IsNotEmpty()
+  // @IsString()
+  // @IsOptional()
+  // title?: string;
+  // @IsNotEmpty()
+  // @IsString()
+  // @IsOptional()
+  // detail?: string;
+  // @IsNotEmpty()
+  // @IsNumber()
+  // @IsOptional()
+  // directorId?: number;
+  // @IsArray()
+  // @ArrayNotEmpty()
+  // @IsNumber(
+  //   {},
+  //   {
+  //     each: true,
+  //   },
+  // )
+  // @IsOptional()
+  // genreIds?: number[];
 }
