f5005c3f376503f496f6dcb25f493fae5636af61
mingdev <alstn67@gmail.com>
2025-12-20 00:29:26 +0900

    refresh 토큰으로 access 토큰 재발급

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from 'src/user/entities/user.entity';
+import { Role, User } from 'src/user/entities/user.entity';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { ConfigService } from '@nestjs/config';
@@ -24,7 +24,11 @@ export class AuthService {
       throw new BadRequestException('토큰 포맷이 잘못됐습니다');
     }
 
-    const [_, token] = basicSplit;
+    const [basic, token] = basicSplit;
+
+    if (basic.toLowerCase() !== 'basic') {
+      throw new BadRequestException('토큰 포맷이 잘못됐습니다');
+    }
 
     // 추출한 토큰을 base64 디코딩해서 이메일과 비밀번호로 나눈다
 
@@ -43,6 +47,36 @@ export class AuthService {
       email,
       password,
     };
+  }
+
+  async parseBearerToken(rawToken: string, isRefreshToken: boolean) {
+    const bearerSplit = rawToken.split(' ');
+
+    if (bearerSplit.length !== 2) {
+      throw new BadRequestException('토큰 포맷이 잘못됐습니다');
+    }
+
+    const [bearer, token] = bearerSplit;
+
+    if (bearer.toLowerCase() !== 'bearer') {
+      throw new BadRequestException('토큰 포맷이 잘못됐습니다');
+    }
+
+    const payload = await this.jwtService.verifyAsync(token, {
+      secret: this.configService.getOrThrow<string>('ACCESS_TOKEN_SECRET'),
+    });
+
+    if (isRefreshToken) {
+      if (payload.type !== 'refresh') {
+        throw new BadRequestException('Refresh 토큰을 입력해주세요');
+      }
+    } else {
+      if (payload.type !== 'access') {
+        throw new BadRequestException('Access 토큰을 입력해주세요');
+      }
+    }
+
+    return payload;
   }
 
   // rawToken -> "Basic $token"
@@ -86,7 +120,7 @@ export class AuthService {
     return user;
   }
 
-  async issueToken(user: User, isRefreshToken: boolean) {
+  async issueToken(user: { id: number; role: Role }, isRefreshToken: boolean) {
     // JWT 토큰 환경변수 값 가져오기
     const refreshTokenSecret = this.configService.get<string>(
       'REFRESH_TOKEN_SECRET',
