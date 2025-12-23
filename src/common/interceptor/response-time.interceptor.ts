33ac97f12b99cea24a39a6ecfe2c157d417c9026
mingdev <alstn67@gmail.com>
2025-12-23 16:32:42 +0900

    response time interceptor 간단 구현

---
+++ src/common/interceptor/response-time.interceptor.ts
@@ -0,0 +1,36 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { delay, tap } from 'rxjs/operators';
+
+@Injectable()
+export class ResponseTimeInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<any> | Promise<Observable<any>> {
+    const req = context.switchToHttp().getRequest();
+
+    const reqTime = Date.now();
+
+    return next.handle().pipe(
+      // delay(1000),
+      tap(() => {
+        const respTime = Date.now();
+        const diff = respTime - reqTime;
+
+        if (diff > 1000) {
+          console.log(`!!!TIME OUT!!! [${req.method} ${req.path}] ${diff}ms`);
+          throw new InternalServerErrorException(`시간이 너무 오래 걸렸습니다`);
+        } else {
+          console.log(`[${req.method} ${req.path}] ${diff}ms`);
+        }
+      }),
+    );
+  }
+}
