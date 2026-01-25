fcbea22a2502df336123358826a13da88684ea61
mingdev <alstn67@gmail.com>
2026-01-25 17:05:29 +0900

    temp 폴더 하루 이상된 파일 삭제 스케줄러 적용(Cron은 테스트 후 미실행)

--- src/common/tasks.service.ts
+++ src/common/tasks.service.ts
@@ -1,12 +1,51 @@
 import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
+// 디렉토리 안에 있는 모든 파일 목록을 읽어오는 모듈
+import { readdir, unlink } from 'fs/promises';
+import { join, parse } from 'path';
 
 @Injectable()
 export class TasksService {
   constructor() {}
 
-  @Cron('* * * * * *')
-  logEveerySecond() {
-    console.log('1초마다 실행되는 작업입니다.');
+  // @Cron('* * * * * *')
+  // logEveerySecond() {
+  //   console.log('1초마다 실행되는 작업입니다.');
+  // }
+
+  // @Cron('* * * * * *')
+  async eraseOrphanFiles() {
+    const files = await readdir(join(process.cwd(), 'public', 'temp'));
+
+    const deleteFilesTargets = files.filter((file) => {
+      // 파일 이름에서 확장자 제거
+      const filename = parse(file).name;
+
+      const split = filename.split('_');
+
+      if (split.length !== 2) {
+        return true;
+      }
+
+      try {
+        const date = +new Date(parseInt(split[split.length - 1]));
+        const aDayInMilSec = 24 * 60 * 60 * 1000;
+
+        const now = +new Date();
+
+        return now - date > aDayInMilSec;
+      } catch (e) {
+        return true;
+      }
+    });
+    // for (const file of deleteFilesTargets) {
+    //   await unlink(join(process.cwd(), 'public', 'temp', file));
+    // }
+
+    await Promise.all(
+      deleteFilesTargets.map((x) =>
+        unlink(join(process.cwd(), 'public', 'temp', x)),
+      ),
+    );
   }
 }
