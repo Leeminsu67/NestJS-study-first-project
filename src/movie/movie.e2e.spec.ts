4a8abb76d5918cfd28a0a0a3f73b34d8df4f93ec
mingdev <alstn67@gmail.com>
2026-02-01 21:58:28 +0900

    e2e 테스트 세팅

---
+++ src/movie/movie.e2e.spec.ts
@@ -0,0 +1,37 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import request from 'supertest';
+import { App } from 'supertest/types';
+import { AppModule } from 'src/app.module';
+
+describe('MovieController (e2e)', () => {
+  let app: INestApplication<App>;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transformOptions: {
+          enableImplicitConversion: true,
+        },
+      }),
+    );
+    await app.init();
+  });
+
+  describe('[GET /movie]', () => {
+    it('should get all movies', async () => {
+      const { body, statusCode, error } = await request(
+        app.getHttpServer(),
+      ).get('/movie');
+
+      expect(statusCode).toBe(200);
+    });
+  });
+});
