ae35dd65099cf4a50bdb65e3cbe977cc1e319b8d
mingdev <alstn67@gmail.com>
2026-02-02 00:23:56 +0900

    movie 모듈 엔드투엔드 테스트 코드 작성

--- src/movie/movie.e2e.spec.ts
+++ src/movie/movie.e2e.spec.ts
@@ -3,13 +3,14 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from 'src/app.module';
-import { User } from 'src/user/entities/user.entity';
+import { Role, User } from 'src/user/entities/user.entity';
 import { Director } from 'src/director/entitie/director.entity';
 import { Movie } from './entity/movie.emtity';
 import { Genre } from 'src/genre/entities/genre.entity';
 import { DataSource } from 'typeorm';
 import { MovieDetail } from './entity/movie-detail.entity';
 import { MovieUserLike } from './entity/movie-user-like.entity';
+import { AuthService } from 'src/auth/auth.service';
 
 describe('MovieController (e2e)', () => {
   let app: INestApplication<App>;
@@ -19,6 +20,8 @@ describe('MovieController (e2e)', () => {
   let directors: Director[];
   let movies: Movie[];
   let genres: Genre[];
+
+  let token: string;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -101,9 +104,17 @@ describe('MovieController (e2e)', () => {
     );
 
     await movieRepository.save(movies);
+
+    const authService = moduleFixture.get<AuthService>(AuthService);
+    token = await authService.issueToken(
+      { id: users[0].id, role: Role.admin },
+      false,
+    );
   });
 
   afterAll(async () => {
+    // 너무 빨리 연결과 앱 연결을 종료하면 이슈 로그가 남기 때문에 500ms 정도 기다린다
+    await new Promise((resolve) => setTimeout(resolve, 500));
     await dataSource.destroy(); // DB 연결 종료
     await app.close(); // 앱 종료
   });
@@ -115,6 +126,179 @@ describe('MovieController (e2e)', () => {
       ).get('/movie');
 
       expect(statusCode).toBe(200);
+      expect(body).toHaveProperty('data');
+      expect(body).toHaveProperty('nextCursor');
+      expect(body).toHaveProperty('count');
+
+      expect(body.data).toHaveLength(5);
+    });
+  });
+
+  describe('[GET /movie/recent]', () => {
+    it('should get recent movies', async () => {
+      const { body, statusCode } = await request(app.getHttpServer())
+        .get('/movie/recent')
+        .set('authorization', `Bearer ${token}`);
+
+      expect(statusCode).toBe(200);
+      expect(body).toHaveLength(10);
+    });
+  });
+
+  describe('[GET /movie/{id}]', () => {
+    it('should get movie by id', async () => {
+      const movieId = movies[0].id;
+      const { body, statusCode } = await request(app.getHttpServer())
+        .get(`/movie/${movieId}`)
+        .set('authorization', `Bearer ${token}`);
+
+      expect(statusCode).toBe(200);
+      expect(body.id).toBe(movieId);
+    });
+
+    it('should throw 404 error if movie does not exist', async () => {
+      const movieId = 9999999;
+
+      const { body, statusCode } = await request(app.getHttpServer())
+        .get(`/movie/${movieId}`)
+        .set('authorization', `Bearer ${token}`);
+
+      expect(statusCode).toBe(404);
+    });
+  });
+
+  describe('[POST /movie]', () => {
+    it('should create movie', async () => {
+      // 모킹을 하는데 실제 값을 받는것처럼 하는 예시
+      const {
+        body: { fileName },
+      } = await request(app.getHttpServer())
+        .post(`/common/video`)
+        .set('authorization', `Bearer ${token}`)
+        .attach(`video`, Buffer.from('test'), 'movie.mp4')
+        .expect(201);
+
+      const dto = {
+        title: 'Test Movie',
+        detail: 'Test Movie Detail',
+        directorId: directors[0].id,
+        genreIds: genres.map((x) => x.id),
+        movieFileName: fileName,
+      };
+
+      const { body, statusCode } = await request(app.getHttpServer())
+        .post(`/movie`)
+        .set('authorization', `Bearer ${token}`)
+        .send(dto);
+
+      expect(statusCode).toBe(201);
+      expect(body).toBeDefined();
+      expect(body.title).toBe(dto.title);
+      expect(body.detail.detail).toBe(dto.detail);
+      expect(body.director.id).toBe(dto.directorId);
+      expect(body.genres.map((x) => x.id)).toEqual(dto.genreIds);
+      expect(body.movieFilePath).toContain(fileName);
+    });
+  });
+
+  describe('[PATCH /movie/{id}]', () => {
+    it('should udpate movie if exists', async () => {
+      const dto = {
+        title: 'Updated Test Movie',
+        detail: 'Updated Test Movie Detail',
+        directorId: directors[0].id,
+        genreIds: [genres[0].id],
+      };
+
+      const movieId = movies[0].id;
+
+      const { body, statusCode } = await request(app.getHttpServer())
+        .patch(`/movie/${movieId}`)
+        .set('authorization', `Bearer ${token}`)
+        .send(dto);
+
+      expect(statusCode).toBe(200);
+      expect(body).toBeDefined();
+      expect(body.title).toBe(dto.title);
+      expect(body.detail.detail).toBe(dto.detail);
+      expect(body.director.id).toBe(dto.directorId);
+      expect(body.genres.map((x) => x.id)).toEqual(dto.genreIds);
+    });
+  });
+
+  describe('[DELETE /movie/{id}]', () => {
+    it('should delete existing movie', async () => {
+      const movieId = movies[0].id;
+
+      const { body, statusCode } = await request(app.getHttpServer())
+        .delete(`/movie/${movieId}`)
+        .set('authorization', `Bearer ${token}`);
+
+      expect(statusCode).toBe(200);
+    });
+
+    it('should throw 404 error if movie does not exist', async () => {
+      const movieId = 999999;
+
+      const { body, statusCode } = await request(app.getHttpServer())
+        .delete(`/movie/${movieId}`)
+        .set('authorization', `Bearer ${token}`);
+
+      expect(statusCode).toBe(404);
+    });
+  });
+
+  describe('[POST /movie/{id}/like]', () => {
+    it('should like a movie', async () => {
+      const movieId = movies[3].id;
+
+      const { body, statusCode } = await request(app.getHttpServer())
+        .post(`/movie/${movieId}/like`)
+        .set('authorization', `Bearer ${token}`);
+
+      expect(statusCode).toBe(201);
+
+      expect(body).toBeDefined();
+      expect(body.isLike).toBe(true);
+    });
+    it('should cancle like a movie', async () => {
+      const movieId = movies[3].id;
+
+      const { body, statusCode } = await request(app.getHttpServer())
+        .post(`/movie/${movieId}/like`)
+        .set('authorization', `Bearer ${token}`);
+
+      expect(statusCode).toBe(201);
+
+      expect(body).toBeDefined();
+      expect(body.isLike).toBeNull();
+    });
+  });
+
+  describe('[POST /movie/{id}/dislike]', () => {
+    it('should dislike a movie', async () => {
+      const movieId = movies[3].id;
+
+      const { body, statusCode } = await request(app.getHttpServer())
+        .post(`/movie/${movieId}/dislike`)
+        .set('authorization', `Bearer ${token}`);
+
+      expect(statusCode).toBe(201);
+
+      expect(body).toBeDefined();
+      expect(body.isLike).toBe(false);
+    });
+    it('should cancle dislike a movie', async () => {
+      const movieId = movies[3].id;
+
+      const { body, statusCode } = await request(app.getHttpServer())
+        .post(`/movie/${movieId}/dislike`)
+        .set('authorization', `Bearer ${token}`);
+
+      expect(statusCode).toBe(201);
+
+      expect(body).toBeDefined();
+      expect(body.isLike).toBeNull();
     });
   });
 });
